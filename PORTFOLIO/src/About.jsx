33a6d9f88dfa4cdf3d434ae63995f67c109af634
TMOUHSIN <136186343+TMOUHSIN@users.noreply.github.com>
2024-09-02 18:02:05 +0100

    Update About.jsx

--- PORTFOLIO/src/About.jsx
+++ PORTFOLIO/src/About.jsx
@@ -3,15 +3,15 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import picture from './assets/p.svg'
 
 function About(){
-    const downloadFile = () => {
-        const url = '/C:\Users\KIRIGAYA\Desktop\portfolio\PORTFOLIO\public\MouhsinTanani_CV.pdf'; // Path to the PDF file in the public directory
-        const link = document.createElement('a');
-        link.href = url;
-        link.download = 'MouhsinTanani_CV.pdf';
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link); // Clean up
-      };
+const downloadFile = () => {
+  const url = '/MouhsinTanani_CV.pdf'; // Path to the PDF file in the public directory
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = 'MouhsinTanani_CV.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link); // Clean up
+};
     return(
         <>
         <div className="about">
